0e62ef3e4bec943e7d9ce61a0cb2ff39847f755b
Junwei Wang <wakemecn@gmail.com>
2012-03-22 18:10:37 +0800

    modified:   ../Cpabe.java 	modified:   Bswabe.java 	modified:   BswabeMsk.java

---
+++ src/bswabe/BswabePrv.java
@@ -0,0 +1,10 @@
+package bswabe;
+
+import java.util.ArrayList;
+
+import it.unisa.dia.gas.jpbc.Element;
+
+public class BswabePrv {
+	Element d;					/* G_2 */
+	ArrayList<BswabePrvComp> comps;	/* BswabePrvComp */
+}
---
+++ src/bswabe/BswabePrvComp.java
@@ -0,0 +1,15 @@
+package bswabe;
+
+import it.unisa.dia.gas.jpbc.Element;
+
+public class BswabePrvComp {
+	/* these actually get serialized */
+	String attr;
+	Element d;					/* G_2 */
+	Element dp;				/* G_2 */
+	
+	/* only used during dec */
+	int used;
+	Element z;					/* G_1 */
+	Element zp;				/* G_1 */
+}
