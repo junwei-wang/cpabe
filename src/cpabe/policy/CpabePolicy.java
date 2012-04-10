9bed0de38bbae09fb830b75721aed01bc20bb17d
Junwei Wang <wakemecn@gmail.com>
2012-04-10 08:55:47 +0800

    删除：      policy/CpabePolicy.java 	删除：      policy/LangPolicy.java 	删除：      policy/SizedInteger.java

--- src/cpabe/policy/CpabePolicy.java
+++
@@ -1,9 +0,0 @@
-package cpabe.policy;
-
-import bswabe.BswabePolicy;
-
-public class CpabePolicy {
-	int k; /* one if leaf, otherwise threshold */
-	String attr; /* attribute string if leaf, null otherwise */
-	BswabePolicy[] children; /* null for leaf */
-}
--- src/cpabe/policy/SizedInteger.java
+++
@@ -1,6 +0,0 @@
-package cpabe.policy;
-
-public class SizedInteger {
-	long	value;
-	int bits;
-}
