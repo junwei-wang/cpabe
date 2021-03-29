baf73142a7586a7af0972323cc7f342d124c663d
netsugo <netsugo@users.noreply.github.com>
2021-03-29 14:17:17 +0900

    added apis

--- cpabe-api/src/main/java/co/junwei/cpabe/Cpabe.java
+++ cpabe-api/src/main/java/co/junwei/cpabe/Cpabe.java
@@ -2,6 +2,8 @@ package co.junwei.cpabe;
 import co.junwei.cpabe.policy.LangPolicy;
 import it.unisa.dia.gas.jpbc.Element;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
@@ -21,6 +23,20 @@ public class Cpabe {
 	 * @author Junwei Wang(wakemecn@gmail.com)
 	 */
 
+	public final int SETUP_PUBLIC = 0;
+	public final int SETUP_MASTER = 1;
+
+	public byte[][] setup() {
+		BswabePub pub = new BswabePub();
+		BswabeMsk msk = new BswabeMsk();
+		Bswabe.setup(pub, msk);
+
+		byte[] publicKey = SerializeUtils.serializeBswabePub(pub);
+		byte[] masterKey = SerializeUtils.serializeBswabeMsk(msk);
+
+		return new byte[][] { publicKey, masterKey };
+	}
+
 	public void setup(String pubfile, String mskfile) throws IOException,
 			ClassNotFoundException {
 		byte[] pub_byte, msk_byte;
@@ -35,6 +51,14 @@ public class Cpabe {
 		/* store BswabeMsk into mskfile */
 		msk_byte = SerializeUtils.serializeBswabeMsk(msk);
 		Common.spitFile(mskfile, msk_byte);
+	}
+
+	public byte[] keygen(byte[] publicKey, byte[] masterKey, String attribute) throws NoSuchAlgorithmException {
+		BswabePub pub = SerializeUtils.unserializeBswabePub(publicKey);
+		BswabeMsk msk = SerializeUtils.unserializeBswabeMsk(pub, masterKey);
+		String[] parsedAttribute = LangPolicy.parseAttribute(attribute);
+		BswabePrv prv = Bswabe.keygen(pub, msk, parsedAttribute);
+		return SerializeUtils.serializeBswabePrv(prv);
 	}
 
 	public void keygen(String pubfile, String prvfile, String mskfile,
@@ -57,6 +81,30 @@ public class Cpabe {
 		/* store BswabePrv into prvfile */
 		prv_byte = SerializeUtils.serializeBswabePrv(prv);
 		Common.spitFile(prvfile, prv_byte);
+	}
+
+	private static byte[] packCpabe(byte[] cphBuf, byte[] aesBuf) throws IOException {
+		// store data
+		// mlen(4byte:int),mbuf,cphlen(4byte),cphbuf,aeslen(4byte),aesBuf
+		byte[] mBuf = new byte[0];
+		ByteArrayOutputStream stream = Common.writeCpabeData(mBuf, cphBuf, aesBuf);
+		byte[] byteArray = stream.toByteArray();
+		stream.close();
+		return byteArray;
+	}
+
+	public byte[] encrypt(byte[] publicKey, String policy, byte[] plain) throws Exception {
+		BswabePub pub = SerializeUtils.unserializeBswabePub(publicKey);
+		BswabeCphKey keyCph = Bswabe.enc(pub, policy);
+		BswabeCph cph = keyCph.cph;
+		Element element = keyCph.key;
+		if (cph == null) {
+			throw new RuntimeException("Error happened while encrypting");
+		} else {
+			byte[] cphBuf = SerializeUtils.bswabeCphSerialize(cph);
+			byte[] aesBuf = AESCoder.encrypt(element.toBytes(), plain);
+			return packCpabe(cphBuf, aesBuf);
+		}
 	}
 
 	public void enc(String pubfile, String policy, String inputfile,
@@ -91,6 +139,29 @@ public class Cpabe {
 		aesBuf = AESCoder.encrypt(m.toBytes(), plt);
 		// PrintArr("element: ", m.toBytes());
 		Common.writeCpabeFile(encfile, cphBuf, aesBuf);
+	}
+
+	private static byte[][] UnpackCpabe(byte[] packed) throws IOException {
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(packed);
+		return Common.readCpabeData(inputStream);
+	}
+
+	public byte[] decode(byte[] publicKey, byte[] privateKey, byte[] encrypted) throws Exception {
+		int BUF_AES = 0;
+		int BUF_CPH = 1;
+		byte[][] tmp = UnpackCpabe(encrypted);
+		byte[] aesBuf = tmp[BUF_AES];
+		byte[] cphBuf = tmp[BUF_CPH];
+		BswabePub pub = SerializeUtils.unserializeBswabePub(publicKey);
+		BswabeCph cph = SerializeUtils.bswabeCphUnserialize(pub, cphBuf);
+		BswabePrv prv = SerializeUtils.unserializeBswabePrv(pub, privateKey);;
+		BswabeElementBoolean beb = Bswabe.dec(pub, prv, cph);
+
+		if (beb.b) {
+			return AESCoder.decrypt(beb.e.toBytes(), aesBuf);
+		} else {
+			throw new RuntimeException("Decrypt error: " + beb.e.toString());
+		}
 	}
 
 	public void dec(String pubfile, String prvfile, String encfile,
